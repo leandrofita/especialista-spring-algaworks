76988e06a2e216dc52e483fe5cf6a5af88c011de
Leandro Fita <leandrocfita@gmail.com>
2024-04-10 19:50:22 -0300

    adicioando o callback do flyway antes de cada teste para fazer o after migrate

--- src/test/java/com/algaworks/algafood/CozinhaControllerApiTestsIT.java
+++ src/test/java/com/algaworks/algafood/CozinhaControllerApiTestsIT.java
@@ -4,11 +4,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,13 +21,23 @@ class CozinhaControllerApiTestsIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
 	private static final String PATH = "/cozinhas";
+	
+	@BeforeEach
+	void setup() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = PATH;
+		
+		flyway.migrate();
+	}
 	
 	@Test
 	void deveRetornarStatus200_QuandoConsultarCozinhas() {
 		given()
-			.basePath(PATH)
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
@@ -34,14 +48,22 @@ class CozinhaControllerApiTestsIT {
 	@Test
 	void deveConter4Cozinhas_QuandoConsultarCozinhas() {
 		given()
-			.basePath(PATH)
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
-			.body("", hasSize(4))
-			.body("nome", hasItems("Indiana", "Tailandesa"));
+			.body("", hasSize(4));
 	}
 
+	@Test
+	void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{\" nome\": \"Chinesa\"}")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
 }
